93660c397e8c7bc6b73b0ba26c6625270c73dd51
TawfiqAhmedRafi <rafi70722@gmail.com>
2025-11-21 08:53:52 +0600

    Delete Parcel

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express();
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 3000
 
 // middleware
@@ -32,19 +32,26 @@ async function run() {
         if(email){
             query.senderEmail= email ;
         }
-        const cursor = parcelsCollection.find(query);
+        const options = {sort :{createdAt: -1}}
+        const cursor = parcelsCollection.find(query,options);
         const result = await cursor.toArray();
         res.send(result)
 
     })
     app.post('/parcels', async (req,res)=>{
         const parcel = req.body ;
+        parcel.createdAt = new Date();
         const result = await parcelsCollection.insertOne(parcel);
         res.send(result)
 
     })
 
-   
+   app.delete('/parcels/:id',async(req,res)=>{
+    const id = req.params.id;
+    const query ={_id: new ObjectId(id)}
+    const result = await parcelsCollection.deleteOne(query);
+    res.send(result);
+   })
 
 
 
