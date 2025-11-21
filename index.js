36d39569cb056aeafe1dc36f9b615871b4a2f32e
TawfiqAhmedRafi <rafi70722@gmail.com>
2025-11-21 07:03:08 +0600

    create parcel get api

--- index.js
+++ index.js
@@ -27,6 +27,14 @@ async function run() {
 
     // parcel api
     app.get('/parcels', async (req,res)=>{
+        const query = {}
+        const {email} = req.query;
+        if(email){
+            query.senderEmail= email ;
+        }
+        const cursor = parcelsCollection.find(query);
+        const result = await cursor.toArray();
+        res.send(result)
 
     })
     app.post('/parcels', async (req,res)=>{
